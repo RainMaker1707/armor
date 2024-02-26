5743eaacaeaa528e3c79b673822da1a700812c98
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-02-26 15:39:59 +0100

    refactor + iso_format_checker

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ from os import listdir
 
 from src.threat.threat import Threat
 from src.mitigation.mitigation import Mitigation
+from src.util import iso_format_checker
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     args = parser.parse_args()
 
-    test = Threat()
-    test.from_scratch(name="test", behavior="test")
-    print(test.get_add_date())
-
     json_path = "data/JSON/"
     json_list = list(filter(lambda e: e!="template.json" ,listdir(json_path)))
-    print(json_list)
 
     xml_path = "data/XML"
     xml_list = list(filter(lambda e: e!="template.xml" ,listdir(xml_path)))
--- src/threat/threat.py
+++ src/threat/threat.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import json
+from src.util import from_json_to_dict, from_xml_to_dict, iso_format_checker
 
 class Threat():
 
@@ -7,12 +7,14 @@ class Threat():
     __name = str()
     __fullname = str()
     __add_date = str()
+    __last_modified = str()
     __first_seen_date = str()
     __behavior = list()
     __type = str()
     __vector = list()
     __impact = list()
     __mitigation_ids = list()
+    __CVE = str()
 
 
     def __init__(self):
@@ -30,44 +32,87 @@ class Threat():
             self.__first_seen_date= first_seen_date
 
 
-    def from_json(self, raw_file):
-        raw_data = dict()
-        with open(raw_file, 'r') as file:
-            raw_data = json.load(file)
-        self.__ID = raw_data["ID"]
-        self.__name = raw_data["name"]
-        self.__fullname = raw_data["fullname"]
-        self.__first_seen_date = raw_data["first_seen"]
-        self.__behavior = raw_data["behavior"]
-        self.__mitigation_ids = raw_data["mitigation_ids"]
-        # TODO: Type must be type object
-        self.__type = raw_data["type"]
-        # TODO: Vector must be vector object
-        self.__vector = raw_data["vector"]
-        self.__impact = raw_data["impact"]
-        self.__add_date = str(datetime.today()).split(" ")[0]
-
+    def from_json(self, file):
+        raw_data = from_json_to_dict(file)
+        self.assign(raw_data)
 
 
     def from_xml(self, file):
+        raw_data = from_xml_to_dict(file)
+        self.assign(raw_data)
+
+
+    def from_stix(file):
         pass
+
+
+    def assign(self, dic):
+        self.__ID = dic.get("ID")
+        self.__name = dic.get("name")
+        self.__fullname = dic.get("fullname")
+        if not iso_format_checker(dic.get("first_seen")):
+            return ValueError("ISO format date error in first_seen: excepted -> YYYY-MM-DD  has -> " + dic.get("first_seen"))
+        self.__first_seen_date = dic.get("first_seen")
+        if not iso_format_checker(dic.get("last_modified")):
+            return ValueError("ISO format date error last_modified: excepted -> YYYY-MM-DD  has -> " + dic.get("last_modified"))
+        self.__last_modified = dic.get("last_modified")
+        self.__behavior = dic.get("behavior")
+        self.__mitigation_ids = dic.get("mitigation_ids")
+        # TODO: Type must be type object
+        self.__type = dic.get("type")
+        # TODO: Vector must be vector object
+        self.__vector = dic.get("vector")
+        self.__impact = dic.get("impact")
+        self.__add_date = str(datetime.today()).split(" ")[0]
+        self.__CVE = dic.get("CVE")
 
 
     def get_add_date(self):
         return self.__add_date
-    
+
+
     def get_first_date(self):
         return self.__first_seen_date
     
+
+    def get_behavior(self):
+        return self.__behavior
+
+    def get_name(self):
+        return self.__name
+    
+
+    def get_type(self):
+        return self.__type
+    
+
+    def get_cve(self):
+        return self.__CVE
+    
+
+    def get_vector(self):
+        return  self.__vector
+    
+
+    def get_impact(self):
+        return self.__impact
+    
+
+    def get_mitigations_ids(self):
+        return self.__mitigation_ids
+
+
     def __str__(self):
-        text = f'ID:          {self.__ID},\n'
-        text += f'Name:        {self.__name},\n'
-        text += f'FullName:    {self.__fullname},\n'
-        text += f'First seen:  {self.__first_seen_date},\n'
-        text += f'Added date:  {self.__add_date},\n'
-        text += f'Behavior:    {self.__behavior},\n'
-        text += f'Type:        {self.__type},\n'
-        text += f'Vector:      {self.__vector},\n'
-        text += f'Impact:      {self.__impact},\n'
-        text += f'Mitigation:  {self.__mitigation_ids},\n'
+        text = f'ID:            {self.__ID},\n'
+        text += f'Name:          {self.__name},\n'
+        text += f'FullName:      {self.__fullname},\n'
+        text += f'First seen:    {self.__first_seen_date},\n'
+        text += f'Last modified: {self.__last_modified}\n'
+        text += f'Added date:    {self.__add_date},\n'
+        text += f'Behavior:      {self.__behavior},\n'
+        text += f'Type:          {self.__type},\n'
+        text += f'Vector:        {self.__vector},\n'
+        text += f'Impact:        {self.__impact},\n'
+        text += f'Mitigation:    {self.__mitigation_ids},\n'
+        text += f'CVE:           {self.__CVE}\n'
         return text
